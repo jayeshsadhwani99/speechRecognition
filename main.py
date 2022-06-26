22ff2fd104343b37310eed6c8e317fd20be56e1a
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-26 21:39:24 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# We can transcribe audio using assemblyAI
+
+# Steps:
+# 1. Upload the file to Assembly AI
+# 2. Start the transcription
+# 3. Wait for the transcription to finish (keep polling the API)
+# 4. Get the transcription (save)
